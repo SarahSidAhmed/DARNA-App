eaf1488245ac6a800bf533f496aac9be6f35bd29
Sarah Sid <sid33585@gmail.com>
2024-03-11 18:49:23 +0100

    Security File (Sarah)

---
+++ DARNAMob/app/src/main/java/com/example/darnamob/Extensions.kt
@@ -0,0 +1,19 @@
+package com.example.darnamob
+
+import java.security.MessageDigest
+
+fun String.toSHA256(): String{
+    val HEX_CHARS = "0123456789ABCDEF"
+    val digest = MessageDigest.getInstance("SHA-256").digest(this.toByteArray())
+    return digest.joinToString(
+        separator = "",
+        transform = { a->
+            String(
+                charArrayOf(
+                    HEX_CHARS[a.toInt() shr 4 and 0x0f],
+                    HEX_CHARS[a.toInt() and 0x0f]
+                )
+            )
+        }
+    )
+}
