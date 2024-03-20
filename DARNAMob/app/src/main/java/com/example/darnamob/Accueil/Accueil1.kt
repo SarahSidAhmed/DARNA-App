27bf70a6f313bc2cdaba8bb642416dae41636e82
malek <divamichaelis@gmail.com>
2024-03-20 17:09:13 +0100

    MALAK

---
+++ DARNAMob/app/src/main/java/com/example/darnamob/Accueil/Accueil1.kt
@@ -0,0 +1,30 @@
+package com.example.darnamob.Accueil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.darnamob.R
+import android.widget.Button
+import android.widget.TextView
+import android.content.Intent
+import android.widget.ImageView
+
+
+
+
+class Accueil1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_accueil1)
+        val button: Button = findViewById(R.id.button1)
+        button.setOnClickListener {
+            val intent = Intent(this, Accueil2::class.java)
+            startActivity(intent)
+        }
+
+        val textview: TextView = findViewById(R.id.skip1)
+        textview.setOnClickListener {
+            val intent = Intent(this,Login_Signup::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ DARNAMob/app/src/main/java/com/example/darnamob/Accueil/Accueil3.kt
@@ -0,0 +1,30 @@
+package com.example.darnamob.Accueil
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.darnamob.R
+
+class Accueil3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_accueil3)
+        val button: Button = findViewById(R.id.button3)
+        button.setOnClickListener {
+            val intent = Intent(this, Login_Signup::class.java)
+            startActivity(intent)
+        }
+        val imageview: ImageView = findViewById(R.id.back3)
+        imageview.setOnClickListener {
+            finish()
+        }
+        val textview: TextView = findViewById(R.id.skip3)
+        textview.setOnClickListener {
+            val intent = Intent(this,Login_Signup::class.java)
+            startActivity(intent)
+        }
+    }
+}
