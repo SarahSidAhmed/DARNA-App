70a34a4bd9757b313beff1447ce130b58a520031
LARIBI-SARA <ms_laribi@esi.dz>
2024-04-02 15:28:16 +0100

    Laribi is here again

--- DARNAMob/app/src/main/java/com/example/darnamob/Client/PerfectFeedBack.kt
+++ DARNAMob/app/src/main/java/com/example/darnamob/Client/PerfectFeedBack.kt
@@ -3,10 +3,10 @@ package com.example.darnamob.Client
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.darnamob.R
-//hi
-class AccountClient : AppCompatActivity() {
+
+class PerfectFeedBack : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_account_client)
+        setContentView(R.layout.activity_perfect_feed_back)
     }
 }
