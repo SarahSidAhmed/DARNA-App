2d75ae4b2bdadfd90b3ae2bc1e4ab6cd2d61074d
selssabil <selssabilkadid@gmail.com>
2024-04-06 06:46:38 +0200

    admin front

---
+++ DARNAMob/app/src/main/java/com/example/darnamob/Admin/CreateAccount.kt
@@ -0,0 +1,13 @@
+package com.example.darnamob.Admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.darnamob.R
+import android.widget.Button
+import android.widget.TextView
+import android.content.Intent
+
+class CreateAccount : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_addnewuser)}}
