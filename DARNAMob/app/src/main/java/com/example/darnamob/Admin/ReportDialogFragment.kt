003ca28f190f37697d1bcf6daf0bd7dd9b4123a6
selssabil <selssabilkadid@gmail.com>
2024-04-30 12:44:33 +0200

    admin side

---
+++ DARNAMob/app/src/main/java/com/example/darnamob/Admin/ReportDialogFragment.kt
@@ -0,0 +1,33 @@
+package com.example.darnamob.Admin
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.DialogFragment
+import com.example.darnamob.R
+
+class ReportDialogFragment : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var rootView: View = inflater.inflate(R.layout.card_confirmreport, container,  false)
+         rootView.findViewById<Button>(R.id.noButton).setOnClickListener {
+             dismiss()
+         }
+        rootView.findViewById<Button>(R.id.yesButton).setOnClickListener {
+
+        }
+        return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view?.findViewById<Button>(R.id.noButton)
+    }
+}
